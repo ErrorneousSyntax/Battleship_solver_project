dd2ff81af81dc425344d90e4d7981146660bd882
ErrorneousSyntax <coe.ishikawa24@imperial.ac.uk>
2026-05-18 11:48:34 +0100

    initial skeleton

---
+++ web-app/index.js
@@ -0,0 +1,29 @@
+const Battleship = {}
+
+
+
+
+Battleship.empty_board = function(){
+
+}
+    
+
+/**
+ * Place a battleship onto the grid
+ * @param {Battleship.ship} ship The ship we are placing
+ * @param {Battleship.grid} grids The grid we are placing into
+ * @returns {Battleship.grid} The resultant grids after the ship is placed
+ */
+Battleship.place_ship = function(ship,grids){
+    
+}
+
+
+Battleship.is_grid_free = function(){
+
+}
+
+Battleship.is_hit = function(){
+
+}
+
